6ef26c7ef09fdc17370ebf54ad94b68dbaf68504
Terence Lim <terence.lim@c02v509shv2t.turn.corp>
2019-01-28 11:19:32 -0800

    test

---
+++ precache-manifest.076f0c5c1f893b89b1b91bc4679e8125.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "23e78b1a729653820a79",
+    "url": "/static/js/main.23e78b1a.chunk.js"
+  },
+  {
+    "revision": "a70a57cf9f150c1bf85a",
+    "url": "/static/js/1.a70a57cf.chunk.js"
+  },
+  {
+    "revision": "23e78b1a729653820a79",
+    "url": "/static/css/main.cd163f5b.chunk.css"
+  },
+  {
+    "revision": "fb8bb97fde874e793c58f84a7d7b3ead",
+    "url": "/index.html"
+  }
+];
